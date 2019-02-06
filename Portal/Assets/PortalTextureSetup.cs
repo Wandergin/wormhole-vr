f8259e60ded34d8e5ba174acae2ed609b6fb0fbd
Mario V <m.vinickis@gmail.com>
2019-02-06 18:54:08 +0000

    #2 the transition is now contained within the same world

--- Portal/Assets/PortalTextureSetup.cs
+++ Portal/Assets/PortalTextureSetup.cs
@@ -1,31 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PortalTextureSetup : MonoBehaviour {
-
-	public Camera camera_Local;
-    public Camera camera_Remote;
-
-
-	public Material cameraMat_Backward;
-	public Material cameraMat_Forward;
-
-    // Use this for initialization
-    void Start () {
-		if (camera_Remote.targetTexture != null)
-		{
-            camera_Remote.targetTexture.Release();
-		}
-        camera_Remote.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMat_Backward.mainTexture = camera_Remote.targetTexture;
-
-        if (camera_Local.targetTexture != null)
-        {
-            camera_Local.targetTexture.Release();
-        }
-        camera_Local.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMat_Forward.mainTexture = camera_Local.targetTexture;
-    }
-	
-}
+using System.Collections;using System.Collections.Generic;using UnityEngine;public class PortalTextureSetup : MonoBehaviour {	public Camera camera_Local;    public Camera camera_Remote;	public Material cameraMat_Backward;	public Material cameraMat_Forward;    // Use this for initialization    void Start () {		if (camera_Remote.targetTexture != null)		{            camera_Remote.targetTexture.Release();		}        camera_Remote.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);        cameraMat_Backward.mainTexture = camera_Remote.targetTexture;        if (camera_Local.targetTexture != null)        {            camera_Local.targetTexture.Release();        }        camera_Local.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);        cameraMat_Forward.mainTexture = camera_Local.targetTexture;    }	}
