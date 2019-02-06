0760e6116e684c9f2e4022cd21acd4bd74df8971
Mario V <m.vinickis@gmail.com>
2019-02-06 17:56:21 +0000

    First commit

---
+++ Portal/Assets/PortalCamera_B.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera_B : MonoBehaviour {
+
+	public Transform playerCamera;
+	public Transform localPortal;
+	public Transform remotePortal;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerOffsetFromPortal = playerCamera.position - localPortal.position;
+		transform.position = remotePortal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(localPortal.rotation, remotePortal.rotation)+180;
+       
+
+		Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+		Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+		transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+	}
+}
--- Portal/Assets/PortalTextureSetup.cs
+++ Portal/Assets/PortalTextureSetup.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 
 public class PortalTextureSetup : MonoBehaviour {
 
-	public Camera cameraA;
 	public Camera cameraB;
+    public Camera cameraA_Tunnel;
+    public Camera cameraB_Tunnel;
+
 
 	public Material cameraMatA;
 	public Material cameraMatB;
 
-	// Use this for initialization
-	void Start () {
-		if (cameraA.targetTexture != null)
-		{
-			cameraA.targetTexture.Release();
-		}
-		cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-		cameraMatA.mainTexture = cameraA.targetTexture;
+    public Material cameraMatA_Tunnel;
+    public Material cameraMatB_Tunnel;
 
+    // Use this for initialization
+    void Start () {
 		if (cameraB.targetTexture != null)
 		{
 			cameraB.targetTexture.Release();
 		}
 		cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
 		cameraMatB.mainTexture = cameraB.targetTexture;
-	}
+
+        if (cameraB_Tunnel.targetTexture != null)
+        {
+            cameraB_Tunnel.targetTexture.Release();
+        }
+        cameraB_Tunnel.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatB_Tunnel.mainTexture = cameraB_Tunnel.targetTexture;
+    }
 	
 }
