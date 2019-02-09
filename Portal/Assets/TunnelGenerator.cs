d01a2b37874bc745acbcf33b4a4efda597e8edb6
Mario V <m.vinickis@gmail.com>
2019-02-09 01:43:37 +0000

    #4 #5 The tunnel is now created using ctrl and removed using alt keys, is rotation and position invariant

--- Portal/Assets/TunnelGenerator.cs
+++ Portal/Assets/TunnelGenerator.cs
@@ -1,26 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TunnelGenerator : MonoBehaviour
 {
-    public Transform sourcePortal;
+    public GameObject sourcePortal;
+    public GameObject targetPortal;
     public Transform tunnelWorld;
+    public Transform player;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        targetPortal.SetActive(false);
+        sourcePortal.SetActive(false);
         //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         //cube.transform.localScale = new Vector3(4, 1, 20);
         //cube.transform.position = new Vector3(0, 0.5f, 0);
         //cube.transform.rotation = sourcePortal.rotation;
 
-        tunnelWorld.rotation = sourcePortal.rotation;
+        tunnelWorld.rotation = sourcePortal.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            targetPortal.SetActive(true);
+            sourcePortal.SetActive(true);
+
+            Vector3 viewport = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 5.0f));
+            Vector3 viewportTarget = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 40.0f));
+
+            sourcePortal.transform.position = new Vector3(viewport.x, 2.0f, viewport.z);
+            targetPortal.transform.position = new Vector3(viewportTarget.x, 2.0f, viewportTarget.z);
+
+            sourcePortal.transform.rotation = Quaternion.Euler(player.rotation.eulerAngles - new Vector3(0, 180, 0));
+            targetPortal.transform.rotation = Quaternion.Euler(player.rotation.eulerAngles - new Vector3(0, 180, 0));
+
+            print(player.rotation.eulerAngles - new Vector3(0,180,0));
+
+            tunnelWorld.rotation = sourcePortal.transform.rotation;
+
+        }
+        else if (Input.GetButtonDown("Fire2"))
+        {
+            targetPortal.SetActive(false);
+            sourcePortal.SetActive(false);
+        }
     }
 }
