8cec75745cb6cb3abbc74df4d3c7441aebe1e667
VR User <m.vinickis@gmail.com>
2019-03-31 22:18:45 +0100

    #6 #11 #10 #7 Player now can create and walk through tunnels

--- Portal/Assets/TunnelGenerator.cs
+++ Portal/Assets/TunnelGenerator.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
+
 
 public class TunnelGenerator : MonoBehaviour
 {
@@ -9,7 +11,9 @@ public class TunnelGenerator : MonoBehaviour
     public GameObject targetPortal;
     public Transform tunnelWorld;
     public Transform player;
-    
+
+    public SteamVR_Input_Sources handType; // 1
+    public SteamVR_Action_Boolean interactUIAction; // 2
 
     // Start is called before the first frame update
     void Start()
@@ -24,22 +28,31 @@ public class TunnelGenerator : MonoBehaviour
         tunnelWorld.rotation = sourcePortal.transform.rotation;
     }
 
+    public bool getInteractUIAction() // 1
+    {
+        return interactUIAction.GetStateDown(handType);
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+
+
+
+        if (getInteractUIAction())
         {
             targetPortal.SetActive(true);
             sourcePortal.SetActive(true);
 
-            Vector3 viewport = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 5.0f));
-            Vector3 viewportTarget = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 40.0f));
+            Vector3 viewport = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1.0f));
+            Vector3 viewportTarget = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 3.0f));
 
             sourcePortal.transform.position = new Vector3(viewport.x, 2.0f, viewport.z);
             targetPortal.transform.position = new Vector3(viewportTarget.x, 2.0f, viewportTarget.z);
 
-            sourcePortal.transform.rotation = Quaternion.Euler(player.rotation.eulerAngles - new Vector3(0, 180, 0));
-            targetPortal.transform.rotation = Quaternion.Euler(player.rotation.eulerAngles - new Vector3(0, 180, 0));
+            sourcePortal.transform.rotation = Quaternion.Euler(0, player.rotation.eulerAngles.y - 180, 0);
+            targetPortal.transform.rotation = Quaternion.Euler(0, player.rotation.eulerAngles.y - 180, 0);
 
             print(player.rotation.eulerAngles - new Vector3(0,180,0));
 
